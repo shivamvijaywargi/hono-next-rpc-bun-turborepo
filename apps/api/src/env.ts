eefbeb1a91fe5116765041b3ef7e54d1c0fa1a1d
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-07-14 13:03:41 +0530

    wip: integrate drizzle + env validation

---
+++ apps/api/src/env.ts
@@ -0,0 +1,40 @@
+import { z, ZodError } from "zod";
+
+const stringBoolean = z.coerce
+  .string()
+  .transform((val) => {
+    return val === "true";
+  })
+  .default("false");
+
+const EnvSchema = z.object({
+  NODE_ENV: z.string().default("development"),
+  DB_HOST: z.string(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
+  DB_PORT: z.coerce.number(),
+  DATABASE_URL: z.string(),
+  DB_MIGRATING: stringBoolean,
+  DB_SEEDING: stringBoolean,
+});
+
+export type EnvSchema = z.infer<typeof EnvSchema>;
+
+try {
+  EnvSchema.parse(Bun.env);
+} catch (error) {
+  if (error instanceof ZodError) {
+    let message = "Missing required values in .env:\n";
+    error.issues.forEach((issue) => {
+      message += issue.path[0] + "\n";
+    });
+    const e = new Error(message);
+    e.stack = "";
+    throw e;
+  } else {
+    console.error(error);
+  }
+}
+
+export default EnvSchema.parse(Bun.env);
